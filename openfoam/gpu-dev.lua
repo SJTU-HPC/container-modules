1e41aceef14b1ecb7e751e9695513f629fc7c24c
shenggan <csg19971016@gmail.com>
2020-10-16 13:26:14 +0800

    [openfoam] add develop enviroment for gpu

---
+++ openfoam/gpu-dev.lua
@@ -0,0 +1,56 @@
+help([==[
+
+Description
+===========
+OpenFOAM (for "Open-source Field Operation And Manipulation") is a C++ toolbox
+for the development of customized numerical solvers, and pre-/post-processing utilities
+for the solution of continuum mechanics problems, most prominently including
+computational fluid dynamics (CFD).
+]==])
+
+whatis("Name: openfoam")
+whatis("Version: v8")
+
+-- conflict(myModuleName(), "openmpi", "chroma", "milc", "qmcpack", "relion")
+
+local dir,file_name=splitFileName(myFileName())
+local image = pathJoin(dir, myModuleVersion()..".sif")
+
+if (subprocess("if [[ -e " .. image .. " ]]; then echo \"exist\"; else echo \"not\"; fi") == "not\n") then
+        LmodError("The container image broken. Contact hpc staff for help.")
+end
+
+local entrypoint_args = ""
+
+-- The absolute path to Singularity is needed so it can be invoked on remote
+-- nodes without the corresponding module necessarily being loaded.
+-- Trim off the training newline.
+local singularity = capture("which singularity | head -c -1")
+
+local container_launch = singularity .. " run " .. image .. " " .. entrypoint_args
+
+local all_bin = subprocess("singularity run " .. image ..  " bash -c \" \
+                                ls /usr/local/cuda/bin && \
+                                ls /opt/OpenFOAM-8/platforms/linux64GccDPInt32Opt/bin && \
+                                cd /opt/OpenFOAM-8/wmake && find * -maxdepth 0 -type f -executable && \
+                                ls /opt/OpenFOAM-8/bin\"")
+local programs = string.gmatch(all_bin, "%S+")
+
+-- Programs to setup in the shell
+for program in programs do
+        set_shell_function(program, container_launch .. " " .. program .. " $@",
+	                            container_launch .. " " .. program .. " $*")
+end
+
+-- setup srun for slurm user
+set_shell_function("srun", 
+        "if [[ $1 == \"--mpi=pmi2\" ]]; then \
+                /usr/bin/srun --mpi=pmi2" .. " " .. container_launch .. " " .. " ${@:2}; \
+        else \
+                /usr/bin/srun ${@:1}; \
+        fi",
+        "if [[ $1 == \"--mpi=pmi2\" ]]; then \
+                /usr/bin/srun --mpi=pmi2" .. " " .. container_launch .. " " .. " ${*:2}; \
+        else \
+                /usr/bin/srun ${@:1}; \
+        fi")
